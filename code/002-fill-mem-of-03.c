6c83b1f98a44ffb4ee7b582a3f048469dbfebe3d
kongkrit <45794368+kongkrit@users.noreply.github.com>
2025-12-01 01:56:16 +0700

    prove of memory continuity

---
+++ code/002-fill-mem-of-03.c
@@ -0,0 +1,15 @@
+#include <stdint.h>
+
+#define ADDR_START 0x4010U
+#define ADDR_END   0xBFF0U
+
+void main(void) {
+  volatile uint16_t *p = (volatile uint8_t *) ADDR_START;
+
+  for (p = ((volatile uint16_t *) ADDR_START); p < ((volatile uint16_t *) ADDR_END); p = p+8) {
+    *p = p;
+  }    
+
+  while (1) {
+  }
+}
